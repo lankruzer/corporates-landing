2b826541764c779d4da35cf120ec6936bfa56835
lan_kruzer <ar-48@yandex,ru>
2018-06-23 10:35:15 +0300

    add screen-services-top and cor-paralax_bottom

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,21 @@ SmoothScroll({
   accelerationMax: 10
 });
 
-var rellax = new Rellax('.cor-parralax-rellax', {
-  speed: 5,
-  center: false,
-  round: true,
-  vertical: true,
-  horizontal: false
+var rellax = new Rellax('.cor-parralax_top .cor-parralax-rellax', {
+  // wrapper: '.cor-parralax_top',
+  speed: 2,
+  // center: true,
+  // round: true,
+  // vertical: true,
+  // horizontal: false
+});
+
+var rellax1 = new Rellax('.cor-parralax_bottom .cor-parralax-rellax', {
+  // wrapper: '.cor-parralax_bottom',
+  speed: 4,
+  // center: false,
+  // round: true,
+  // vertical: true,
+  // horizontal: false
 });
 
